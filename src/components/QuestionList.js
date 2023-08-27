abe0013d4b7963af9a92bb3681caf46cf43fc8d8
Hana Sambora <grace.sambora@gmail.com>
2023-08-26 22:21:02 -0500

    Questions display upon page load

--- src/components/QuestionList.js
+++ src/components/QuestionList.js
@@ -1,10 +1,21 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import QuestionItem from "./QuestionItem";
 
 function QuestionList() {
+  const [questions, setQuestions] = useState([]);
+
+  useEffect(()=>{
+    fetch("http://localhost:4000/questions")
+    .then((response)=>response.json())
+    .then((questions)=>setQuestions(questions))
+  }, [])
+
   return (
     <section>
       <h1>Quiz Questions</h1>
-      <ul>{/* display QuestionItem components here after fetching */}</ul>
+      <ul>{questions && questions.map((question)=>{
+        return(<QuestionItem key={question.id} question={question}/>)
+        })}</ul>
     </section>
   );
 }
